032cf1c9a19595d510ac8a4063d238893317f0d0
Dev. Sanches <dev.anaclarasanches@gmail.com>
2024-01-26 10:44:47 -0300

    Complete feature added.

--- gui/todo-list-gui.py
+++ gui/todo-list-gui.py
@@ -6,34 +6,45 @@ label = ps.Text('Type in a To-do:')
 input_box = ps.InputText(tooltip='Enter a todo', key='add')
 add_button = ps.Button('Add')
 edit_button = ps.Button('Edit')
-list_box = ps.Listbox(values=todo_list, key='todos_list', enable_events=True, size=(40, 10))
+exit_button = ps.Button('Exit', tooltip='Exit the program')
+complete_button = ps.Button('Complete')
+list_box = ps.Listbox(values=todo_list, key='todos_list', enable_events=True, size=(40, 10), tooltip='Select a TO-DO '
+                                                                                                     'to complete or '
+                                                                                                     'edit')
 
 window = ps.Window('Todo List App', font=('Helvetica', 13), layout=[
     [label],
     [input_box, add_button],
-    [list_box, edit_button]
+    [list_box, edit_button, complete_button],
+    [exit_button]
 ])
 
 
 while True:
     events, values = window.read()
+
     print(f'EVENT: {events}\nVALUES: {values}')
 
     if events == 'Add':
         new_todo = values['add']
         todo_list.append(new_todo + '\n')
         f.update_file(todo_list)
-
-        #  atualiza a lista inteira
         window['todos_list'].update(values=todo_list)
 
+    #  text box value changes according to the selected list item
     elif events == 'todos_list':
-        window['add'].update(value=values['todos_list'][0])
+        print(values['todos_list'])
+        window['add'].update(value=values['todos_list'][0].strip())
+
+    elif events == 'Complete':
+        completed_todo = values['todos_list'][0]
+        todo_list.remove(completed_todo)
+        f.update_file(todo_list)
+        window['todos_list'].update(values=todo_list)
+        window['add'].update(value='')
 
     elif events == 'Edit':
         to_edit = values['todos_list'][0]
-
-        #  índice do to-do a ser editado na lista de todos
         todo_index = todo_list.index(to_edit)
         new_todo = values['add']
         todo_list[todo_index] = new_todo + '\n'
